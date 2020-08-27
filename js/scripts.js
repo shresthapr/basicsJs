9bebf6f4ad9a2195f6dfcc8ce9657d86dddc7b6e
shresthapr <shresthaprajan38@gmail.com>
2020-08-27 21:02:34 +0300

    assignment 1 COMPLETED

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,34 @@
-function startScript () {
-    console.log("Hello World")
-    document.body.style.backgroundImage = 'url("img/bg.jpg")';
-};
-document.addEventListener('DOMContentLoaded',startScript);
+function startScript() {
+	console.log("Hello World");
+	document.body.style.backgroundImage = 'url("img/bg.jpg")';
 
+	var anything = document.createElement("div");
+	anything.id = "title-wrapper";
+	anything.innerText = "See how it works";
+	document.body.appendChild(anything);
 
-var div = document.createElement('div');
+	document.getElementById("title-wrapper").style.margin = 50;
+	document.getElementById("title-wrapper").style.width = 100;
+	document.getElementById("title-wrapper").style.textAlign = "center";
+	document.getElementById("title-wrapper").style.fontFamily = "impact";
+	document.getElementById("title-wrapper").style.position = "absolute";
+	document.getElementById("title-wrapper").style.textTransform = "uppercase";
 
-div.id = 'title-wrapper';
-div.innerHTML = '<p>Check if it appears</p>';
-var div = document.getElementsByTagName('body');
-document.body.appendChild('div');
+	var aitle = document.createElement("h1");
+	aitle.id = "title";
+	aitle.innerText = "Work under progress...";
+	document.getElementById("title-wrapper").appendChild(aitle);
+	aitle.style.cssText =
+		"width:fit-content; margin: auto; background:black; color:white";
+}
 
-document.getElementById('title-wrapper').style.margin= 50;
-document.getElementById('title-wrapper').style.width= 100;
-document.getElementById('title-wrapper').style.textAlign= "center";
-document.getElementById('title-wrapper').style.fontFamily = "impact";
-document.getElementById('title-wrapper').style.position = "absolute";
-document.getElementById('title-wrapper').style.textTransform = "uppercase";
+document.addEventListener("DOMContentLoaded", startScript);
 
-var h1 = document.createElement("h1");
-h1.id= "title";
-h1.innerHTML = "Work under progress...";
+// var h1 = document.createElement("h1");
+// h1.id = "title";
+// h1.innerHTML = "Work under progress...";
 
-document.getElementsByName('h1').style = "width:fit-content; margin: auto; background:black; color:white";
+// document.getElementsByName("h1").style =
+// 	"width:fit-content; margin: auto; background:black; color:white";
 
-document.div.appendChild("h1");
-
+// document.div.appendChild("h1");
